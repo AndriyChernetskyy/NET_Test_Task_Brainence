ef184b222b31272462bf151545c14bb6044b1cbe
Andriy <andrewusa921@gmail.com>
2018-09-19 14:51:14 +0300

    Done

---
+++ Task/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication8.Models;
+
+namespace WebApplication8.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DbFirstModel _context;
+
+        public HomeController()
+        {
+            _context = new DbFirstModel();
+        }
+
+        public IActionResult Index()
+        {
+            var query = from b in _context.Sentences
+                        select b;
+            return View(query.ToList());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IList<IFormFile> files, string keyword)
+        {
+            foreach (var file in files)
+            {
+                var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                if (fileName.EndsWith(".txt"))
+                {
+                    var result = string.Empty;
+                    using (var reader = new StreamReader(file.OpenReadStream()))
+                    {
+                        result = reader.ReadToEnd();
+                        string[] input = result.Split('.');
+                        for(int i = 0; i < input.Length; i++)
+                        {
+                            if (input[i].ToLower().Contains(keyword.ToLower()))
+                            {
+                                int numberOfOccurances = 0;
+                                string[] input1 = input[i].Split(' ');
+                                for(int j = 0; j < input1.Length; j++)
+                                {
+                                    if(input1[j].ToLower() == keyword.ToLower())
+                                    {
+                                        numberOfOccurances++;
+                                    }
+                                }
+                                _context.Add(new InputSentences {Sentence = input[i], NumberOfOccurrences = numberOfOccurances});
+                                _context.SaveChanges();
+                            }
+                        }
+                    }
+                }
+             }
+
+            return RedirectToAction("Index");
+       }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Task/Models/DBFirstModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8.Models
+{
+    public class DbFirstModel : DbContext
+    {
+        public DbFirstModel()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-L3DGQ0O\SQLEXPRESS;Database=Sentences;Trusted_Connection=True;");
+        }
+
+        public virtual DbSet<InputSentences> Sentences { get; set; }
+    }
+
+    public class InputSentences
+    {
+        [Key]
+        public int Id { get; set; }
+        public int NumberOfOccurrences { get; set; }
+        public string Sentence { get; set; }
+    }
+
+}
+
